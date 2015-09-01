e3d35082df4e7c492eb16cb096276617989160c8
Thomas <thomas@theperfectwedding.nl>
2015-09-01 16:39:57 +0200

    Added the calendar entity support

---
+++ src/Pipelinedeals/CalendarEntry.php
@@ -0,0 +1,19 @@
+<?php
+// add the namespace
+namespace Weblab\Pipelinedeals;
+
+/**
+ * The Pipelinesdeals wrapper class wrapping arround calendar entries from
+ * Pipelinesdeals, making it possible to perform rest operations on the calendar
+ * entity
+ * 
+ * @author Weblab.nl - Thomas Marinissen
+ */
+class CalendarEntity extends \Weblab\Pipelinedeals\Entity {
+    
+    /**
+     * The entity name
+     */
+    const NAME = 'calendar_entries';
+
+}
